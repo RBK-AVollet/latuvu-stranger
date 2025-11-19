566af19b69077159d4dd67c00acc3cf99f44e301
Enzo Dufour <enzo.dufour05@gmail.com>
2025-11-19 12:12:16 +0100

    State machine + Free movement

---
+++ LatuvuStranger/Assets/0_Scripts/StateMachine/GridLocomotionState.cs
@@ -0,0 +1,18 @@
+﻿namespace Latuvu
+{
+    public class GridLocomotionState : BaseState
+    {
+        public GridLocomotionState(PlayerController player) : base(player)
+        { }
+
+        public override void Update()
+        {
+            // Noop    
+        }
+        
+        public override void FixedUpdate()
+        {
+            _player.HandleGridMovement();
+        }
+    }
+}
