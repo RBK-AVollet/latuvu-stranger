4fcfd9554f151743e3b8b9ccf0fae7cc1500df84
Antoine VOLLET <a.vollet@rubika-edu.com>
2025-11-19 18:12:27 +0100

    [ADD] Apparemment il faut supprimer des trucs

--- LatuvuStranger/Assets/0_Scripts/Systems/GridService/Tiles/NumberTile.cs
+++
@@ -1,9 +0,0 @@
-using UnityEngine;
-namespace Latuvu
-{
-    [CreateAssetMenu(menuName = "Latuvu/Tiles/Number Tile")]
-    public class NumberTile : GameTile
-    {
-        public int Value = 0;
-    }
-}
