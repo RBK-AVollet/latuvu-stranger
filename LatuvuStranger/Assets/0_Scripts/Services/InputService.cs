9c75a29fef3c434d4a4efda7c13c7bd377a1b486
Enzo Dufour <enzo.dufour05@gmail.com>
2025-11-20 12:26:02 +0100

    [EDIT] PlayerController & InputService & GameService

--- LatuvuStranger/Assets/0_Scripts/Services/InputService.cs
+++ LatuvuStranger/Assets/0_Scripts/Services/InputService.cs
@@ -17,28 +17,32 @@ namespace Latuvu
             
             _moveAction = _playerInput.actions.FindAction("Move");
             _interactionAction = _playerInput.actions.FindAction("Interact");
+            
+            _playerInput.actions.Enable();
         }
 
         public void RegisterMoveAction(Action<InputAction.CallbackContext> callback)
         {
-            Debug.Log("RegisterMoveAction");
+            Debug.Log($"RegisterMoveAction {callback.Method.Name}");
             
             _moveAction.started += callback;
         }
 
         public void UnregisterMoveAction(Action<InputAction.CallbackContext> callback)
         {
-            _moveAction.canceled -= callback;
+            _moveAction.started -= callback;
         }
         
         public void RegisterWandInteraction(Action<InputAction.CallbackContext> callback)
         {
+            Debug.Log($"RegisterWandInteraction {callback.Method.Name}");
+            
             _interactionAction.started += callback;
         }
         
         public void UnregisterWandInteraction(Action<InputAction.CallbackContext> callback)
         {
-            _interactionAction.canceled -= callback;
+            _interactionAction.started -= callback;
         }
     }
 }
